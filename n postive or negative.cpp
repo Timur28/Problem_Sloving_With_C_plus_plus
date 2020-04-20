9ba8d4fcd7c53bd36bd1841ac562555fce65b867
Muhammad Taimoor <62425639+Timur28@users.noreply.github.com>
2020-04-20 11:12:01 +0500

    C++ program to tell number is + || -
    Write a C++ program that takes inputs from the user in the form of a number and outputs whether number is positive or negative

---
+++ n postive or negative.cpp
@@ -0,0 +1,22 @@
+//taask 1
+//topic 3 IF then else
+#include <iostream>
+using namespace std;
+int main( )
+{
+int user_input;
+cout << "This program tell you wheather the given number is postive or negative\n ";
+cout << "Enter a Number:";
+cin >> user_input;
+if (user_input > 0)
+{
+cout << "The given number is positive ";
+}
+else
+{cout <<"The given number is negative";
+}
+cout << endl;
+cout << "That's all! Stay Blessed\n";
+return 0;
+}
+
