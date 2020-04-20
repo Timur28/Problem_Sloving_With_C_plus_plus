a8850448027ae0b54734253da94956d18f38493b
Muhammad Taimoor <62425639+Timur28@users.noreply.github.com>
2020-04-20 12:06:11 +0500

    C++ program to Print natural numbers in des-order
    Write a C++ program to print natural numbers 1 to 10 in descending order.

---
+++ Natural Numbers.cpp
@@ -0,0 +1,14 @@
+//a program to print natural numbers 1 to 10 in descending order.
+#include <iostream>
+using namespace std;
+int main ()
+{
+	cout << "This program print natural numbers 1 to 10 in descending order.\n";
+	for (int n=10;n>0;n--)
+	{
+		cout<<n<<endl;
+	}
+	cout<<"That's All\n";
+	cout << "=================================";
+	return 0;
+}
